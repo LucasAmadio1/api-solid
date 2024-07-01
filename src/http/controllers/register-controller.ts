3ae2c5b8623c55660a293e0626810c12ca69abc6
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-06-30 21:24:26 -0300

    .

---
+++ src/http/controllers/register-controller.ts
@@ -0,0 +1,25 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { registerService } from '../services/register-service'
+
+export async function register(req: FastifyRequest, res: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    await registerService({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return res.status(409).send()
+  }
+
+  return res.status(201).send()
+}
